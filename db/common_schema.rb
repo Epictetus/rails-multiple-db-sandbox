4a8fad82a3e693a303274bc0df9f9cfa34dafb7e
youichiro <cinnamon416@gmail.com>
2020-10-27 00:18:54 +0900

    bin/rails db:migrate

--- db/common_schema.rb
+++ db/common_schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_10_26_151304) do
+
+  create_table "schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
