92de89214d7c9c8de148d7f13afe17a397a730cc
youichiro <cinnamon416@gmail.com>
2020-10-27 00:07:29 +0900

    db schemaの修正

--- db/school_schema.rb
+++ db/school_schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_141956) do
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
