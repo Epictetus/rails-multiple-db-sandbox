18976e61b3ee1e02afe686803a95afa8e61f8c73
youichiro <cinnamon416@gmail.com>
2020-10-31 19:14:26 +0900

    demoを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :schools, only: [:index, :show, :create, :update, :destroy]
-  get '/demo', to: 'demo#index'
 end
